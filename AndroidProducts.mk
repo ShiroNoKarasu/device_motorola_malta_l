e202ad3ff3c2c9ab8f9f7e3e33a690026376b1b2
DoctorStrange96 <thestrangedoctor.1996@gmail.com>
2025-07-12 16:12:21 -0300

    Initial bringup for `malta_l` (Moto E7 Power arm32)

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_malta.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_malta-user \
+    lineage_malta-userdebug \
+    lineage_malta-eng
